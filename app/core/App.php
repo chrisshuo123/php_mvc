b769a4b49faa1fc009b3bad95a1e87ba347351e6
chrisshuo123 <chr1z100@gmail.com>
2025-09-06 05:26:54 +0700

    PHP #3: Routing - Add another debug format with clean array -> value directory view using echo <pre> and foreach loop, that works like the explode(delimiter, string) style

--- app/core/App.php
+++ app/core/App.php
@@ -8,12 +8,21 @@ class App {
     }
 
     public function parseURL() {
+
         if(isset($_GET['url']) ) {
             $url = rtrim($_GET['url'], '/'); // rtrim agar slash '/' dibagian paling depannya URL bisa ngilang saat ditekan enter
             $url = filter_var($url, FILTER_SANITIZE_URL); // Membersihkan URL dari karakter2 yang ngacau utk menghindar hacker
             $url = explode('/', $url); // Dibagian ini, URL nya kita pecah berdasarkan tanda slash '/' menggunakan fungsi 'explode'.  awalnya '$url = explode(delimiter, string)', yang delimiter itu slash '/', jadi slash-slashnya ilang, jadi string2 nya berubah menjadi elemen array, dari yang mana? dari '$url'. 
             // array => key itu yang kita butuhkan utk nanti kita jalankan controller dan method kita.  Sampai sini, kita sudah berhasil melakukan routing dan indeks kita.
+            
+             // Debug dengan format rapi
             echo "<pre>";
+            echo "array (size=" . count($url) . ")\n";
+            foreach ($url as $key => $value) {
+                echo "  $key => string '$value' (length=" . strlen($value) . ")\n";
+            }
+            echo "</pre>";
+            
             return $url;
         }
     }
