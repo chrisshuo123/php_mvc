77425365199b753e9182e319b4e8fae033ae5c0b
chrisshuo123 <chr1z100@gmail.com>
2025-09-06 04:39:14 +0700

    PHP #3: Routing - add require_once autoload.php from app/core folder, and code the working directory debug view due to lack of php's version to show current working directory on the index.php front-end view.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
+require_once __DIR__ . '/../app/core/autoload.php';
 
 require_once __DIR__ . '/../app/init.php';
+
+// Debug Working Directory
+echo "Current working directory: " . getcwd() . "<br>";
+echo "__DIR__: " . __DIR__ . "<br>";
+echo "Script path: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
 
 $app = new App;
