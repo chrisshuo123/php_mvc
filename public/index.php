ead4ee2728eca70e9bd66a5d9f3fd2e16a233e5b
chrisshuo123 <chr1z100@gmail.com>
2025-10-04 01:25:37 +0700

    re-arrange my PHP MVC codes recently from branch main to branch phpmvc1_persiapan (in english phpmvc part 1: preparing, to make this more easy so I can understand my previous progress made last

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
-require_once __DIR__ . '/../app/core/autoload.php';
 
-require_once __DIR__ . '/../app/init.php';
-
-// Debug Working Directory
-echo "Current working directory: " . getcwd() . "<br>";
-echo "__DIR__: " . __DIR__ . "<br>";
-echo "Script path: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
+require_once '../app/init.php';
 
 $app = new App;
