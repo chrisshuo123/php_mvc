2974f1b9c95c907aff644c43e3c5be630cd551d3
chrisshuo123 <chr1z100@gmail.com>
2025-10-04 00:56:08 +0700

    give cmd // on the 'Debug Working Directory' section that recently use as var_dump alternative, where the var_dump now calls via the 'core/App.php' directory on the 'Params' section inside the construct function

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../app/core/autoload.php';
 require_once __DIR__ . '/../app/init.php';
 
 // Debug Working Directory
-echo "Current working directory: " . getcwd() . "<br>";
-echo "__DIR__: " . __DIR__ . "<br>";
-echo "Script path: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
+//echo "Current working directory: " . getcwd() . "<br>";
+//echo "__DIR__: " . __DIR__ . "<br>";
+//echo "Script path: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
 
 $app = new App;
