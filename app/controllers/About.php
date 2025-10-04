ec7440e1c2042c0349d17327e4a7ce8f75c16650
chrisshuo123 <chr1z100@gmail.com>
2025-10-05 05:50:47 +0700

    in app/controllers/About.php: Adds 'controller' extension making this About controller as child. Like in the commit made, modify the echo into directory search, especially for the index method we refine the  by using the associative array by calling each params in the index method, store it in ->view('about/index', ), so when it calls the view directory, the  also being sent there.

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,17 @@
 <?php
 
-class About {
-    public function index($name = 'Chrissuo', $hobby = 'Kyokushin') {
-        echo "Halo, saya $name, Hobi saya adalah $hobby";
+class About extends Controller {
+    // pada contoh ini, method index membutuhkan data, maka pada $this->view, harus tambahkan $data
+    public function index($nama = 'Chrissuo', $pekerjaan = 'Kyokushin', $umur = 30) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('about/index', $data);
+        //echo "Halo, saya $name, Hobi saya adalah $hobby.  Saya berumur $umur tahun.";
     }
+    // Pada contoh ini, method page tidak dibutuhkan data.  Jika tidak, pada $this->view tdk perlu ditambahkan apa2.
     public function page() {
-        echo 'About/page';
+        $this->view('about/page');
+        //echo 'About/page';
     }
 }
