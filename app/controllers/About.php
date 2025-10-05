d9738be6b9450ddc630cbbae325f45125512f871
chrisshuo123 <chr1z100@gmail.com>
2025-10-06 02:17:45 +0700

    Same as Home.php controllers, add new ->view above & below about/page and about/index method to call each header and footer, and add ['judul'] (which stands for ['title'] in english), to call 'About Us' and 'Page' into the views as 'Halaman About Us' and 'Halaman Pages' ('Halaman' means 'Page' in English) Respectively to the headers page

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -6,12 +6,18 @@ class About extends Controller {
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
+        $data['judul'] = 'About Us';
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
+        $this->view('templates/footer');
         //echo "Halo, saya $name, Hobi saya adalah $hobby.  Saya berumur $umur tahun.";
     }
     // Pada contoh ini, method page tidak dibutuhkan data.  Jika tidak, pada $this->view tdk perlu ditambahkan apa2.
     public function page() {
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
         //echo 'About/page';
     }
 }
