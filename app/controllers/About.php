963198008ae76289bcd36e5cfc8c546de7b01888
chrisshuo123 <chr1z100@gmail.com>
2025-10-08 05:09:50 +0700

    add all files from backup-before-fix branch into this main branch

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,23 @@
 <?php
 
-class About {
-    public function index($name = 'Chrissuo', $hobby = 'Kyokutin Karate') {
-        echo "Halo, saya $name, Hobi saya adalah $hobby";
+class About extends Controller {
+    // pada contoh ini, method index membutuhkan data, maka pada $this->view, harus tambahkan $data
+    public function index($nama = 'Chrissuo', $pekerjaan = 'Kyokushin', $umur = 30) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Us';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+        //echo "Halo, saya $name, Hobi saya adalah $hobby.  Saya berumur $umur tahun.";
     }
+    // Pada contoh ini, method page tidak dibutuhkan data.  Jika tidak, pada $this->view tdk perlu ditambahkan apa2.
     public function page() {
-        echo 'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+        //echo 'About/page';
     }
 }
