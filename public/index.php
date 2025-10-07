7a96ac7862b8744932615a18c9dd1f217b0b256c
chrisshuo123 <chr1z100@gmail.com>
2025-10-08 04:58:04 +0700

    in public/index.php, add 'DIR' for the ../app/init.php directory (with cmd // from the phpmvc2_routing (previously forgot to merge to main)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once '../app/init.php';
+require_once __DIR__ . '/../app/init.php';
+
+// Debug Working Directory
+//echo "Current working directory: " . getcwd() . "<br>";
+//echo "__DIR__: " . __DIR__ . "<br>";
+//echo "Script path: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
 
 $app = new App;
