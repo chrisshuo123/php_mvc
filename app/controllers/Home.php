298fec79cc19c8581323eed43cc3ff56ba12523d
chrisshuo123 <chr1z100@gmail.com>
2025-10-24 21:46:55 +0700

    add some little explanation on line 7

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,7 @@ class Home extends Controller {
     public function index() {
         // Pada data ini, kita bisa kirimin data ke view:
         $data['judul'] = "Home";
-        // Contoh, saya maunya dikirim ke models dulu, bukan view:
+        // Contoh, saya maunya dikirim ke models dulu bukan views pada data nama, maka:
         $data['nama'] = $this->model('User_model')->getUser(); // ke class model, method 'getUser()'
         $this->view('templates/header', $data); // Dimana judul 'Home' dikirim ke view pada header
         $this->view('home/index', $data); // << nama dikirim ke models akan dibawa kesini
