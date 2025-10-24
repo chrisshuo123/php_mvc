e9196b888a5a42fed4b790affd5c280112bc39ce
chrisshuo123 <chr1z100@gmail.com>
2025-10-24 21:16:18 +0700

    add private  and , create the constructor to connect with localhost/phpmyadmin (note: port in 3307, not 3306) to get all data via getAllMahasiswa() method

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,42 +1,24 @@
 <?php
 
 class Mahasiswa_model {
-    private $mhs = [
-        [
-            "nama" => "Andi Wijaya",
-            "nrp" => "112310001",
-            "email" => "andi.wijaya@email.com",
-            "jurusan" => "Teknik Informatika"
-        ],
-        [
-            "nama" => "Sari Damayanti",
-            "nrp" => "112310002",
-            "email" => "sari.damayanti@mail.com",
-            "jurusan" => "Sistem Informasi"
-        ],
-        [
-            "nama" => "Budi Santoso",
-            "nrp" => "112310003",
-            "email" => "budi.santoso99@example.com",
-            "jurusan" => "Teknik Elektro"
-        ],
-        [
-            "nama" => "Citra Lestari",
-            "nrp" => "112310004",
-            "email" => "c.lestari@students.ac.id",
-            "jurusan" => "Manajemen Bisnis"
-        ],
-        [
-            "nama" => "Dimas Pratama",
-            "nrp" => "112310005",
-            "email" => "dimas.pratama@mail.com",
-            "jurusan" => "Desain Komunikasi Visual"
-        ]
-    ];
+    private $dbh; // DB Handler
+    private $stmt;
+
+    public function __construct() {
+        // Data Source Handler
+        $dsn = 'mysql:host=localhost:3307;dbname=phpmvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa() {
-        return $this->mhs;
-        
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 
