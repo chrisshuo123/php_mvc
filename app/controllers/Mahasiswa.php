f8d3961b2605e41fd9d14afd36478389ba54953b
chrisshuo123 <chr1z100@gmail.com>
2025-10-26 06:07:27 +0700

    revise method tambah in controllers mahasiswa

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -20,14 +20,26 @@ class Mahasiswa extends Controller {
     }
 
     // Buat Insert Data Mahasiswa
+    // public function tambah() {
+    //     //var_dump($_POST); // << Ini buat memastikan datanya terkirim semua
+    //     // Kalau nilainya > 0 berarti ada baris baru yg ditambahkan.
+    //     if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) { //<< Jika dilihat pada ($POST) > '0', kan kita butuh angka disini ya? kalau method tambahDataMahasiswa di Mahasiswa_model menghasilkan angka 1, berarti ada 1 baris baru yg ditambahkan kedalam tabel kita. Nah skrg belum. Utk itu, kita perlu mengembalikan nilai di Mahasiswa_models. Tpi sblmnya, bikin DB Wrapper dulu di core/Database.php berupa method rowCount(). Stelahnya, return kan pada mahasiswa_model di dlm method tambahDataMahasiswa.
+    //         header('Location: ' . BASEURL . '/mahasiswa');
+    //         exit;
+    //     }
+    // } // Ini sama kyk kita lakukan di PHP Dasar, tapi pakai yg gaya MVC. Lanjut tambaa method tambahDataMahasiswa pada models.
+
     public function tambah() {
-        //var_dump($_POST); // << Ini buat memastikan datanya terkirim semua
-        // Kalau nilainya > 0 berarti ada baris baru yg ditambahkan.
-        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) { //<< Jika dilihat pada ($POST) > '0', kan kita butuh angka disini ya? kalau method tambahDataMahasiswa di Mahasiswa_model menghasilkan angka 1, berarti ada 1 baris baru yg ditambahkan kedalam tabel kita. Nah skrg belum. Utk itu, kita perlu mengembalikan nilai di Mahasiswa_models. Tpi sblmnya, bikin DB Wrapper dulu di core/Database.php berupa method rowCount(). Stelahnya, return kan pada mahasiswa_model di dlm method tambahDataMahasiswa.
+        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
-    } // Ini sama kyk kita lakukan di PHP Dasar, tapi pakai yg gaya MVC. Lanjut tambaa method tambahDataMahasiswa pada models.
+    }
 }
 
 ?>
